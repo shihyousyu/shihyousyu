df2c5d086ea1a3504c606ca2867b1fe638735fa1
Syu <125783860+shihyousyu@users.noreply.github.com>
2025-05-01 03:13:22 +0800

    Update update_waka.py

--- update_waka.py
+++ update_waka.py
@@ -5,38 +5,31 @@ README = "README.md"
 START = "<!--START_SECTION:waka-->"
 END = "<!--END_SECTION:waka-->"
 
-def fetch(username):
-    api_url = f"https://{'wakatime.com'}/api/v1/users/{username}/stats"
-    response = requests.get(api_url)
-    if response.status_code != 200:
-        raise Exception("Failed to fetch WakaTime stats.")
-    return response.json()["data"]
+api_url = f"https://{'wakatime.com'}/api/v1/users/Syu/stats"
+response = requests.get(api_url).json()["data"]
 
-def format(data):
-    l = [f"Coding Time：{data['categories'][0]['text']}"]
-    l.append("## Language：")
-    for i in data['languages'][:5]:
-        name = i['name']
-        time = i['text']
-        percent = i['percent']
-        l.append(f"- {name}: {time} ({percent:.1f}%)")
-    return "\n".join(l)
+l = [f"Coding Time：{response['categories'][0]['text']}"]
+l.append("## Language：")
+for i in response['languages'][:5]:
+    name = i['name']
+    time = i['text']
+    percent = i['percent']
+    l.append(f"- {name}: {time} ({percent:.1f}%)")
+s = "\n".join(l)
 
-def update(content):
-    with open(README, "r", encoding="utf-8") as f:
-        old = f.read()
+with open(README, "r", encoding="utf-8") as f:
+    old = f.read()
 
-    start = old.find(START)
-    end = old.find(END)
+start = old.find(START)
+end = old.find(END)
 
-    new = old[:start + len(START)] + "\n" + content + "\n" + old[end:]
-    with open(README, "w", encoding="utf-8") as f:
-        f.write(new)
+new = old[:start + len(START)] + "\n" + s + "\n" + old[end:]
+with open(README, "w", encoding="utf-8") as f:
+    f.write(new)
 
 def main():
     data = fetch("Syu")
     result = format(data)
-    print(result)
     #update(result)
 
 if __name__ == "__main__":
