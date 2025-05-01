a3ec2f833e425013dfbb55ed3ab5115a42ce8d1f
Syu <125783860+shihyousyu@users.noreply.github.com>
2025-05-01 09:06:19 +0800

    Update update_waka.py

--- update_waka.py
+++ update_waka.py
@@ -14,7 +14,7 @@ for i in res['languages'][:5]:
     time = i['text']
     percent = i['percent']
     l.append(f"- {name}: {time} ({percent:.1f}%)")
-    l.append("#" * int(percent) + "-" * 100 - int(percent))
+    l.append("#" * int(percent) + "-" * (100 - int(percent)))
 s = "\n".join(l)
 
 with open(README, "r", encoding="utf-8") as f:
